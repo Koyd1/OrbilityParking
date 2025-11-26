6766e4e66aa5f2f844672ee818e8438c08cbd26c
Really-true-dude <andrekor2004@gmail.com>
2025-11-26 16:45:49 +0200

    Small fixes to running script

--- bootstrap_all_windows.ps1
+++ bootstrap_all_windows.ps1
@@ -6,29 +6,32 @@ $ErrorActionPreference = "Stop"
 $pythonCmd = Get-Command "python" -ErrorAction SilentlyContinue
 
 if (-not $pythonCmd) {
-    Write-Host "Python not found. Downloading the 3.10.11 x64 installer..."
-    $pythonUrl = "https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe"
-    $installer = Join-Path $env:TEMP "python-3.10.11-amd64.exe"
-    Invoke-WebRequest -Uri $pythonUrl -OutFile $installer
+    $pythonCmd = Get-Command "py" -ErrorAction SilentlyContinue
+    if ($pythonCmd) {
+        Write-Host "Python not found. Downloading the 3.10.11 x64 installer..."
+        $pythonUrl = "https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe"
+        $installer = Join-Path $env:TEMP "python-3.10.11-amd64.exe"
+        Invoke-WebRequest -Uri $pythonUrl -OutFile $installer
 
-    Write-Host "Installing Python for the current user only..."
-    $installDir = Join-Path $env:LocalAppData "Programs\Python\Python310"
-    $args = @(
-        "/quiet",
-        "InstallAllUsers=0",
-        "TargetDir=$installDir",
-        "Include_pip=1",
-        "PrependPath=0",
-        "Shortcuts=0"
-    )
-    Start-Process -FilePath $installer -ArgumentList $args -Wait -NoNewWindow
+        Write-Host "Installing Python for the current user only..."
+        $installDir = Join-Path $env:LocalAppData "Programs\Python\Python310"
+        $args = @(
+            "/quiet",
+            "InstallAllUsers=0",
+            "TargetDir=$installDir",
+            "Include_pip=1",
+            "PrependPath=0",
+            "Shortcuts=0"
+        )
+        Start-Process -FilePath $installer -ArgumentList $args -Wait -NoNewWindow
 
-    # Обновляем путь к только что установленному интерпретатору
-    $pythonExe = Join-Path $installDir "python.exe"
-    if (-not (Test-Path $pythonExe)) {
-        Write-Error "Python not found after install in: $pythonExe"
+        # Обновляем путь к только что установленному интерпретатору
+        $pythonExe = Join-Path $installDir "python.exe"
+        if (-not (Test-Path $pythonExe)) {
+            Write-Error "Python not found after install in: $pythonExe"
+        }
+        $env:PATH = "$installDir;$installDir\Scripts;$env:PATH"
     }
-    $env:PATH = "$installDir;$installDir\Scripts;$env:PATH"
 } 
 else {
     Write-Host "Found Python: $($pythonCmd.Source)"
--- run.bat
+++ run.bat
@@ -8,6 +8,11 @@ REM --- PowerShell scripts to execute ---
 set PS1_FILE1=%SCRIPT_DIR%bootstrap_all_windows.ps1
 set PS1_FILE2=%SCRIPT_DIR%run_windows.ps1
 
+REM --- Running Visual C++ installer. File VC_redist.x64.exe should be in root directory
+REM --- File can be found here: https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170#latest-supported-redistributable-version
+echo === Running Visual C++ installer. VC_redist.x64.exe... ===
+VC_redist.x64.exe
+
 echo === Setting ExecutionPolicy for CurrentUser ===
 powershell -NoProfile -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force"
 echo ExecutionPolicy set to RemoteSigned
